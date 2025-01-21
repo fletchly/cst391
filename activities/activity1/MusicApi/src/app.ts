cd6dd0e716abb06a347211d8c612f6ec1e88c304
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-01-21 13:22:37 -0700

    Add MySQL connector

--- activities/activity1/MusicApi/src/app.ts
+++ activities/activity1/MusicApi/src/app.ts
@@ -2,12 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import albumsRouter from "./albums/albums.routes";
 import artistsRouter from "./artists/artists.routes";
+import logger from "./middleware/logger.middleware";
+import cors from "cors";
+import helmet from "helmet";
 
 dotenv.config();
 
 // Declare and initialize
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+
+if (process.env.NODE_ENV == "development") {
+  app.use(logger);
+  console.log(process.env.GREETING + " in dev mode");
+}
 
 app.use("/", [albumsRouter, artistsRouter]);
 
