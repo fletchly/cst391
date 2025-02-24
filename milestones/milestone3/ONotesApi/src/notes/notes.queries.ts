186434425b6a2939bcb3a2aa7bd65bf5b941fdc8
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-02-23 22:45:17 -0700

    Add comments

--- milestones/milestone3/ONotesApi/src/notes/notes.queries.ts
+++ milestones/milestone3/ONotesApi/src/notes/notes.queries.ts
@@ -1,7 +1,19 @@
 export const notesQueries = {
+    // Read all notes
     readNotes: `SELECT BIN_TO_UUID(id) AS id, title, content, created, updated FROM onotes.notes;`,
-    readNotesById: `SELECT BIN_TO_UUID(id) AS id, title, content, created, updated FROM onotes.notes WHERE id = ?;`,
-    createNote: `INSERT into onotes.notes (title, content, created, updated) values(?,?,?,?);`,
-    updateNote: `UPDATE onotes.notes SET title = ?, content = ?, created = ?, updated = ? WHERE id = ?;`,
-    deleteNote: `DELETE FROM onotes.notes WHERE id = ?;`,
+
+    // Read notes by id
+    // The note ID is stored in the DB as a binary(16) type, and converted to and from a UUID when queried
+    readNotesById: `SELECT BIN_TO_UUID(id) AS id, title, content, created, updated FROM onotes.notes WHERE id = UUID_TO_BIN(?)`,
+
+    // Create note
+    // Upon creation, the note's created and updated values are both set to the current date and time
+    createNote: `INSERT into onotes.notes (title, content, created, updated) values(?,?,NOW(),NOW());`,
+
+    // Update note
+    // Upon updating, the note's updated value is set to the current date and time
+    updateNote: `UPDATE onotes.notes SET title = ?, content = ?, created = ?, updated = NOW() WHERE id = UUID_TO_BIN(?);`,
+
+    // Delete note
+    deleteNote: `DELETE FROM onotes.notes WHERE id = UUID_TO_BIN(?);;`,
 }
