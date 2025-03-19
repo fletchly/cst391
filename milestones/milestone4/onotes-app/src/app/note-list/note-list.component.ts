e6b07bd1fa8144c62152f180873d1409fc2e0a31
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-03-19 15:40:33 -0700

    Add note list component

---
+++ milestones/milestone4/onotes-app/src/app/note-list/note-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-note-list',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './note-list.component.html',
+  styleUrl: './note-list.component.css'
+})
+export class NoteListComponent {
+
+}
