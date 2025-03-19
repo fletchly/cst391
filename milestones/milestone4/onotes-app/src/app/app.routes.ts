ef645ca5ae9a5b32dd9e8d3a9e16e457b3885a4c
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-03-19 15:39:26 -0700

    Add styles

--- milestones/milestone4/onotes-app/src/app/app.routes.ts
+++ milestones/milestone4/onotes-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {NoteListComponent} from './note-list/note-list.component';
+import {NoteDisplayComponent} from './note-display/note-display.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: NoteListComponent
+  },
+  {
+    path: 'note/:id',
+    title: 'Note',
+    component: NoteDisplayComponent
+  },
+];
