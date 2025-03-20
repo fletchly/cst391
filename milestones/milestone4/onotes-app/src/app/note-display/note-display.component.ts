61024fb8abb4ecd217edab47e32983f990bf00cd
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-03-19 20:49:32 -0700

    Add note display with styles

--- milestones/milestone4/onotes-app/src/app/note-display/note-display.component.ts
+++ milestones/milestone4/onotes-app/src/app/note-display/note-display.component.ts
@@ -1,5 +1,5 @@
 import {Component, inject} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, RouterLink} from '@angular/router';
 import {NoteService} from '../service/note.service';
 import {Note} from '../model/note.model';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
@@ -8,14 +8,17 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/
   selector: 'app-note-display',
   imports: [
     ReactiveFormsModule,
+    RouterLink,
   ],
   templateUrl: './note-display.component.html',
   styleUrl: './note-display.component.css'
 })
 export class NoteDisplayComponent {
+
   route: ActivatedRoute = inject(ActivatedRoute);
   noteService: NoteService = inject(NoteService);
 
+  noteId: string | null = this.route.snapshot.paramMap.get('id');
   note: Note | null = null;
   formattedCreated: Date | null = null;
   formattedUpdated: Date | null = null;
@@ -26,17 +29,19 @@ export class NoteDisplayComponent {
   });
 
   constructor() {
-    this.noteService.getNoteById(this.route.snapshot.paramMap.get('id'), (note: Note) => {
+    this.noteService.getNoteById(this.noteId, (note: Note) => {
       this.note = note;
-      this.formattedCreated = new Date(note.created);
-      this.formattedUpdated = new Date(note.updated);
+      this.formatDates();
 
       this.noteForm.setValue({
         title: note.title,
         content: note.content,
       });
-    });
 
+      const textarea = document.getElementById('content') as HTMLTextAreaElement;
+      textarea.style.height = 'auto';
+      textarea.style.height = textarea.scrollHeight + 'px';
+    });
   }
 
   handleSubmit() {
@@ -47,11 +52,27 @@ export class NoteDisplayComponent {
     }
 
     this.noteService.updateNote(this.note, () => {});
+    this.refreshNote();
   }
 
   autoGrow(event: Event) {
     const textarea = event.target as HTMLTextAreaElement;
     textarea.style.height = 'auto';
     textarea.style.height = textarea.scrollHeight + 'px';
+  }
+
+
+  private refreshNote(): void {
+    this.noteService.getNoteById(this.noteId, (note: Note) => {
+      this.note = note;
+      this.formatDates();
+    });
+  }
+
+  private formatDates(): void {
+    if (this.note) {
+      this.formattedCreated = new Date(this.note.created);
+      this.formattedUpdated = new Date(this.note.updated);
+    }
   }
 }
