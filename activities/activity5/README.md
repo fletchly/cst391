0c136dc712a324d0c30a0ecc47cffc45ad865e01
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-03-24 11:10:59 -0700

    Stopping point 1

---
+++ activities/activity5/README.md
@@ -0,0 +1,13 @@
+# Activity 5
+
+## Stopping Point 1
+
+### Summary
+
+At this point, I've added custom App and Card components to the app. I've also added properties to the Card component to allow dynamic display.
+
+### Screenshots
+
+![](screenshots/a5s1.png)
+*Screenshot 1: Application running with custom card components*
+---
--- activities/activity5/music/src/App.tsx
+++ activities/activity5/music/src/App.tsx
@@ -1,10 +1,31 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Card from "./Card.tsx";
 
 function App() {
   return (
-    <>
-        <div>Hello World!</div>
-    </>
+      <>
+          <h1>Music I like</h1>
+          <Card
+              albumTitle={"Abbey Road"}
+              albumDescription={"The Beatles' 11th studio album"}
+              imageUrl={"https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg"}
+              buttonText={"Listen"}
+          />
+          <Card
+              albumTitle={"Yellow Submarine"}
+              albumDescription={"The Beatles' 10th studio album"}
+              imageUrl={"https://upload.wikimedia.org/wikipedia/en/a/ac/TheBeatles-YellowSubmarinealbumcover.jpg"}
+              buttonText={"Listen"}
+          />
+          <Card
+              albumTitle={"Sgt. Pepper's Lonely Hearts Club Band"}
+              albumDescription={"The Beatles' 8th studio album"}
+              imageUrl={"https://upload.wikimedia.org/wikipedia/en/5/50/Sgt._Pepper%27s_Lonely_Hearts_Club_Band.jpg"}
+              buttonText={"Listen"}
+          />
+      </>
   )
 }
 
