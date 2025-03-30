e4a6d92b9940885a8d712656799f474dc2beffab
Owen Mount <96270460+fletchly@users.noreply.github.com>
2025-03-30 14:29:03 -0700

    Stopping point 2

--- activities/activity5/music/src/App.tsx
+++ activities/activity5/music/src/App.tsx
@@ -1,32 +1,38 @@
-import './App.css'
+import {useState} from 'react';
+import Card from './Card';
+import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import Card from "./Card.tsx";
 
-function App() {
-  return (
-      <>
-          <h1>Music I like</h1>
-          <Card
-              albumTitle={"Abbey Road"}
-              albumDescription={"The Beatles' 11th studio album"}
-              imageUrl={"https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg"}
-              buttonText={"Listen"}
-          />
-          <Card
-              albumTitle={"Yellow Submarine"}
-              albumDescription={"The Beatles' 10th studio album"}
-              imageUrl={"https://upload.wikimedia.org/wikipedia/en/a/ac/TheBeatles-YellowSubmarinealbumcover.jpg"}
-              buttonText={"Listen"}
-          />
-          <Card
-              albumTitle={"Sgt. Pepper's Lonely Hearts Club Band"}
-              albumDescription={"The Beatles' 8th studio album"}
-              imageUrl={"https://upload.wikimedia.org/wikipedia/en/5/50/Sgt._Pepper%27s_Lonely_Hearts_Club_Band.jpg"}
-              buttonText={"Listen"}
-          />
-      </>
-  )
-}
-
-export default App
+const App = () => {
+    const [albumList, setAlbumList] = useState([{
+        artistId: 0,
+        artist: 'The Beatles',
+        title: 'Yellow Submarine',
+        description: 'Yellow Submarine is the tenth studio album by English rock band the Beatles, released on 13 January 1969 in the United States and on 17 January 1969 in the United Kingdom.',
+        year: 1969,
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/TheBeatles-YellowSubmarinealbumcover.jpg/220px-TheBeatles-YellowSubmarinealbumcover.jpg',
+    }, {
+        artistId: 1,
+        artist: 'The Beatles',
+        title: 'Abbey Road',
+        description: 'Yellow Submarine is the tenth studio album by English rock band the Beatles, released on 13 January 1969 in the United States and on 17 January 1969 in the United Kingdom.',
+        year: 1969,
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Beatles_-_Abbey_Road.jpg/220px-Beatles_-_Abbey_Road.jpg',
+    }, {
+        artistId: 2,
+        artist: 'The Beatles',
+        title: 'Let It Be',
+        description: "Let It Be is the twelfth and final studio album by the English rock band the Beatles. It was released on 8 May 1970, almost a month after the group's break-up.",
+        year: 1970,
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/2/25/LetItBe.jpg/220px-LetItBe.jpg',
+    },]);
+    const renderedList = () => {
+        return albumList.map((album) => {
+            return (<Card key={album.title} albumTitle={album.title} albumDescription={album.description} buttonText='OK'
+                          imageUrl={album.image}/>);
+        });
+    };
+    return <div className='container d-flex flex-wrap'>{renderedList()}</div>;
+};
+export default App;
--- activities/activity5/music/src/Card.tsx
+++ activities/activity5/music/src/Card.tsx
@@ -1,7 +1,7 @@
 function Card (props: { albumTitle: string, albumDescription: string, imageUrl: string, buttonText: string }) {
     return (
         <>
-            <div className="card" style={{width: '18rem'}}>
+            <div className="card m-2" style={{width: '18rem'}}>
                 <img src={props.imageUrl} className="card-img-top" alt={props.albumTitle}/>
                 <div className="card-body">
                     <h5 className="card-title">{props.albumTitle}</h5>
